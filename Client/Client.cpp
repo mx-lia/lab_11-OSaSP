0aa2ba31ede2ffa383fa31021dae6d18a06cd4a8
Yuliya Maximchikova <m.yuliyas.00@gmail.com>
2019-10-26 00:22:48 +0300

    Добавьте файлы проекта.

---
+++ Client/Client.cpp
@@ -0,0 +1,88 @@
+﻿#include <stdio.h>
+#include <winsock2.h>
+#include <string>
+#include <iostream>
+
+#pragma warning(disable : 4996)
+#pragma comment(lib, "WS2_32.lib")
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	WORD ver = MAKEWORD(2, 0);
+	WSADATA wsaData;
+	int retVal = 0;
+
+	WSAStartup(ver, (LPWSADATA)&wsaData);
+
+	LPHOSTENT hostEnt;
+
+	hostEnt = gethostbyname("localhost");
+
+	if (!hostEnt)
+	{
+		cout << "Unable to collect gethostbyname\n";
+		WSACleanup();
+		return 1;
+	}
+
+	//Создаем сокет
+	SOCKET clientSock = socket(AF_INET, SOCK_STREAM, 0);
+
+	if (clientSock == SOCKET_ERROR)
+	{
+		cout << "Unable to create socket\n";
+		WSACleanup();
+		return 1;
+	}
+
+	SOCKADDR_IN serverInfo;
+
+	serverInfo.sin_family = AF_INET;
+	serverInfo.sin_addr.s_addr = inet_addr("127.0.0.1");
+	serverInfo.sin_port = htons(2000);
+
+	retVal = connect(clientSock, (LPSOCKADDR)&serverInfo, sizeof(serverInfo));
+	if (retVal == SOCKET_ERROR)
+	{
+		cout << "Unable to connect\n";
+		WSACleanup();
+		return 1;
+	}
+
+	cout << "Connection made sucessfully\n";
+
+	string message;
+	cout << "Enter message for server: \n";
+	cin >> message;
+
+	retVal = send(clientSock, (message.append("\0")).c_str(), sizeof(message), 0);
+
+	if (retVal == SOCKET_ERROR)
+	{
+		cout << "Unable to send\n";
+		getchar();
+		WSACleanup();
+		return 1;
+	}
+
+	string response;
+	retVal = recv(clientSock, (char*)response.c_str(), sizeof(response) , 0);
+
+	if (retVal == SOCKET_ERROR)
+	{
+		cout << "Unable to recv\n";
+		WSACleanup();
+		return 1;
+	}
+
+	cout << "Got the response from server\n" << response << endl;
+	getchar();
+	closesocket(clientSock);
+	WSACleanup();
+
+	return 0;
+}
---
+++ Server/Server.cpp
@@ -0,0 +1,104 @@
+﻿#include <winsock2.h>
+#include <stdio.h>
+#include <string>
+#include <iostream>
+
+#pragma comment(lib, "Ws2_32.lib")
+
+using namespace std;
+
+int main(void)
+{
+	setlocale(LC_ALL, "rus");
+
+	WORD sockVer;
+	WSADATA wsaData;
+	int retVal;
+
+	sockVer = MAKEWORD(2, 0);
+
+	WSAStartup(sockVer, &wsaData);
+
+	//Создаем сокет
+	SOCKET servSock = socket(AF_INET, SOCK_STREAM, 0);
+
+	if (servSock == INVALID_SOCKET)
+	{
+		cout << "Unable to create socket\n";
+		getchar();
+		WSACleanup();
+		return SOCKET_ERROR;
+	}
+	SOCKADDR_IN sin;
+	sin.sin_family = AF_INET;
+	sin.sin_port = htons(2000);
+	sin.sin_addr.s_addr = INADDR_ANY;
+
+	retVal = bind(servSock, (LPSOCKADDR)&sin, sizeof(sin));
+	if (retVal == SOCKET_ERROR)
+	{
+		cout << "Unable to bind\n";
+		WSACleanup();
+		return SOCKET_ERROR;
+	}
+
+	//Пытаемся начать слушать сокет
+	retVal = listen(servSock, 10);
+
+	if (retVal == SOCKET_ERROR)
+	{
+		cout << "Unable to listen\n";
+		WSACleanup();
+		return SOCKET_ERROR;
+	}
+
+	cout << "Waiting client.... \n";
+
+	//Ждем клиента
+	SOCKET clientSock;
+	SOCKADDR_IN clnt;
+	memset(&clnt, 0, sizeof(clnt));
+	int lclnt = sizeof(clnt);
+
+	clientSock = accept(servSock, (sockaddr*)&clnt, &lclnt);
+
+	if (clientSock == INVALID_SOCKET)
+	{
+		cout << "Unable to accept\n";
+		WSACleanup();
+		return SOCKET_ERROR;
+	}
+
+	cout << "Client is connected.... \n";
+
+	string response;
+	retVal = recv(clientSock, (char*)response.c_str(), sizeof(response), 0);
+
+	if (retVal == SOCKET_ERROR)
+	{
+		cout << "Unable to recv\n";
+		return SOCKET_ERROR;
+	}
+
+	cout << "Got the response from client\n" << response << endl;
+	getchar();
+
+	string message;
+	cout << "Enter message for client: \n";
+	cin >> message;
+
+	retVal = send(clientSock, message.append("\0").c_str(), sizeof(message), 0);
+
+	if (retVal == SOCKET_ERROR)
+	{
+		cout << "Unable to send\n";
+		return SOCKET_ERROR;
+	}
+
+	//Закрываем сокет
+	closesocket(clientSock);
+	closesocket(servSock);
+
+	WSACleanup();
+	return 0;
+}
